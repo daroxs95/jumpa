59bd65969061f0edbaade6d6bff13e0298915647
daro <daroxs95@gmail.com>
2024-11-21 22:29:09 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,32 @@
+![Frame 3.png](doc/Frame%203.png)
+
+# Jumpa
+
+Real time generative art and processing + hype gradle project template...ish
+## How to run
+
+```bash
+./gradlew run
+```
+
+## How to build
+
+```bash
+./gradlew build
+```
+
+## Features
+- Real time processing
+- Minim audio library
+- Video playing
+
+## Visualizations
+
+Visuals are intended to be used with DOOMxGUTS music
+and custom Doom Eternal gameplay footage.
+
+## ⚠️ MAC USERS ⚠️
+- Testes on apple silicon M3 chip,
+building the fat jar and running the app works fine
+(probably rosetta is involved), 
+but running the app using the gradle task `run` will probably not work.
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,20 +5,10 @@ plugins {
 }
 
 group = "org.jumpa"
-version = "1.0-SNAPSHOT"
+version = "1.0.1"
 
 application {
     mainClass.set("org.jumpa.Main")
-
-    applicationDefaultJvmArgs = listOf(
-        "-Xms512M",
-        "-Xmx24000M",
-        "-XX:+UseConcMarkSweepGC",
-        "-XX:+UseParNewGC",
-        "-XX:+CMSIncrementalPacing",
-        "-XX:+AggressiveOpts",
-        "-Djava.net.preferIPv4Stack=true"
-    )
 }
 
 repositories {
@@ -29,17 +19,11 @@ repositories {
 }
 
 java {
-//    sourceCompatibility = JavaVersion.VERSION_21
-//    targetCompatibility = JavaVersion.VERSION_21
-
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-
     implementation("org.bytedeco:javacv-platform:1.5.6")
 
     // HYPE - Replace "latest.release" with specific version if necessary
@@ -47,17 +31,13 @@ dependencies {
 
     // Processing libraries
     implementation(files("libs/processing-4.3_library/core.jar"))
-
     implementation("org.jogamp.gluegen:gluegen-rt:2.4.0")
     implementation("org.jogamp.gluegen:gluegen-rt:2.4.0:natives-macosx-universal")
     implementation("org.jogamp.jogl:jogl-all:2.4.0")
     implementation("org.jogamp.jogl:jogl-all:2.4.0:natives-macosx-universal")
 
-    // Minim and related
     implementation("net.compartmental.code:minim:2.2.2")
-
     implementation("org.bytedeco:ffmpeg-platform:5.1.2-1.5.8")
-
 }
 
 tasks.test {
